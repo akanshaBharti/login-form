292cd5515e6e9b73e37af852bdd1bf0b6f657b57
Akansha <akanshabharti1213@gmail.com>
2023-06-11 18:22:59 +0530

    removed errors from inpu.jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link
 } from "react-router-dom";
 
 import Navbar from './components/Navbar';
